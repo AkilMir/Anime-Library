dca3b3721a898da109cf83aa8bf2d4670fb85308
Akil <akilmir8222003@gmail.com>
2024-02-10 21:01:56 -0500

    commit node_modules

--- src/AnimeCard.jsx
+++ src/AnimeCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const AnimeCard = ({anime: { year, images, title, type} }) => {
+const AnimeCard = ({anime: { year, images, title, type, genres} }) => {
     return (
         <div className = "anime">
             <div>
                 <p>{year}</p>
+                <p>{genres[0].name}</p>
             </div>
             <div>
                 <img src={images.jpg.image_url !== 'N/A' ? images.jpg.image_url : 'https://via.placeholder.com/400'} alt={title}/>
